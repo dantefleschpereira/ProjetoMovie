db2394292d5326612b49c9ad198d6b39b5af2d0b
dantefleschpereira <48799984+dantefleschpereira@users.noreply.github.com>
2021-05-06 09:59:21 -0300

    update

--- Persistencia/Entidades/Actor.cs
+++ Persistencia/Entidades/Actor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Persistencia.Entidades
@@ -9,7 +10,11 @@ namespace Persistencia.Entidades
     {
 
         public int ActorId { get; set; }
+
+        [Display(Name = "Ator")]
         public String Name { get; set; }
+
+        [Display(Name = "Data de Nascimento")]
         public DateTime DateBirth { get; set; }
 
         public virtual ICollection<ActorMovie> Characters { get; set; }
--- Persistencia/Entidades/Genre.cs
+++ Persistencia/Entidades/Genre.cs
@@ -8,8 +8,11 @@ namespace Persistencia.Entidades
      public class Genre
     {
         public int GenreId { get; set; }
+
         [Display(Name="Gênero")]
         public string Name { get; set; }
+
+        [Display(Name = "Descrição")]
         public string Description { get; set; }
 
         public virtual ICollection<Movie> Movies { get; set; }
